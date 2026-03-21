71c9c38dac6386feb801af91704dccedb9e9c82a
enigma-dy <oluwagbengaadedire@gmail.com>
2026-03-21 15:06:30 +0100

    fix

--- init.sql
+++ init.sql
@@ -1,1 +1,12 @@
-CREATE DATABASE mytrackr;
+DO $$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'mytrackr') THEN
+      CREATE DATABASE mytrackr
+          WITH OWNER = postgres
+          ENCODING = 'UTF8'
+          LC_COLLATE = 'en_US.utf8'
+          LC_CTYPE = 'en_US.utf8'
+          TEMPLATE = template0
+          CONNECTION LIMIT = -1;
+   END IF;
+END$$;

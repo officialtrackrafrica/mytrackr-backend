6f907614cf3f84c4f2dbd1eabf4ca0198cbc38c0
enigma-dy <oluwagbengaadedire@gmail.com>
2026-07-06 15:08:18 +0100

    update

--- src/admin/entities/support-ticket-reply.entity.ts
+++ src/admin/entities/support-ticket-reply.entity.ts
@@ -26,7 +26,7 @@ export class SupportTicketReply {
   @Column({ type: 'text' })
   message: string;
 
-  @Column({ nullable: true })
+  @Column({ type: 'text', nullable: true })
   attachmentUrl: string | null;
 
   @CreateDateColumn()
